6af2cd160ea734a6d0c88f55c6d69c05e763b614
YaelZamora <ajedrezkasparov@gmail.com>
2024-01-23 12:02:05 -0600

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Yael Javier Zamora Moreno on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
